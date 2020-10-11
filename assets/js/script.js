f989f362db28de3dc039c578861cc4bab9d8f757
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-11 15:55:49 -0500

    updated results column

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
-fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=dd156ef16f803cbff8d4d2bd948e5f9e")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data)
-    })
+// define variables
+var apiKey = "dd156ef16f803cbff8d4d2bd948e5f9e";
+var cityEl = $(".form-control").value;
+
+// when search button is clicked, return search result
+$(".btn").on("click", function(event) {
+    console.log("button was clicked");
+});
+
+// create a function to get weather data
+function getWeather() {
+    fetch("api.openweathermap.org/data/2.5/forecast?q=" + cityEl + "&appid=" + apiKey)
+        .then(function(response) {
+            return response.json();
+        })
+}
 
 
